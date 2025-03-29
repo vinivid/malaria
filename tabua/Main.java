872e35d5abea26ac95b450a417ef7c0974d7f8aa
vinivid <frato.vini@gmail.com>
2025-03-29 16:12:11 -0300

    primeiro exercicio

--- tabua/Main.java
+++ tabua/Main.java
@@ -1,19 +1,28 @@
-import java.util.Vector;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Vector<Integer> tabela_inicial = new Vector<Integer>(1, 2);
+        ArrayList<Integer> tabela_inicial = new ArrayList<Integer>();
 
         Scanner leitor = new Scanner(System.in);
         String numeros_lidos = leitor.nextLine();
         String[] ok = numeros_lidos.split(" ");
 
         for (var i : ok) {
-            tabela_inicial.addElement(Integer.parseInt(i));
+            tabela_inicial.add(Integer.parseInt(i));
         }
+
+        String solucao = leitor.nextLine();
 
         leitor.close();
         Table no = new Table(tabela_inicial);
+        boolean verdade = no.aplicarSolucao(solucao);
+
+        if (verdade == true) {
+            System.out.println("Posicao final: true");
+        } else {
+            System.out.println("Posicao final: false");
+        }
     }
 }
--- tabua/Table.java
+++ tabua/Table.java
@@ -1,15 +1,15 @@
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Table {
-    private Vector<Integer> tab;
+    private ArrayList<Integer> tab;
     private int size;
     private int zero_index;
 
-    public Table(Vector<Integer> jogo) {
+    public Table(ArrayList<Integer> jogo) {
         this.tab = jogo;
         this.size = (int)(Math.sqrt(jogo.size()));
 
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < jogo.size(); ++i) {
             if (tab.get(i) == 0) {
                 this.zero_index = i;
                 break;
@@ -17,7 +17,7 @@ public class Table {
         }
     }
 
-    public void setTable(Vector<Integer> newTable) {
+    public void setTable(ArrayList<Integer> newTable) {
         this.tab = newTable;
     }
 
@@ -34,7 +34,13 @@ public class Table {
             System.out.print("|");
 
             for (int j = 0; j < this.size; ++j) {
-                System.out.printf("%d |", this.tab.get(i * this.size + j));
+                Integer print_val = this.tab.get(i * this.size + j);
+
+                if (print_val <= 9) {
+                    System.out.printf("   %d  |", print_val);
+                } else {
+                    System.out.printf("  %d  |", print_val);
+                }
             }
 
             System.out.print("\n");
@@ -46,29 +52,66 @@ public class Table {
             }
 
             System.out.print("\n");
-        }
+        } 
+        System.out.print("\n");
     }
 
+    private void moveZeroToLoc (int new_pos) {
+        tab.set(zero_index, tab.get(new_pos));
+        tab.set(new_pos, 0); 
+        zero_index = new_pos;
+    }
+
+    //Por algum motivo todos os movimentos são invertidos
     private void up() {
-        
+        if ( (this.zero_index / this.size) == this.size - 1)
+            return;
+
+        int new_pos = this.zero_index + this.size;
+        moveZeroToLoc(new_pos);
     }
 
     private void down () {
+        if ( (this.zero_index / this.size) == 0)
+            return;
 
+        int new_pos = this.zero_index - this.size;
+        moveZeroToLoc(new_pos);
     }
 
     private void left () {
+        if ( (this.zero_index % this.size) == this.size - 1)
+            return;
 
+        int new_pos = this.zero_index + 1;
+        moveZeroToLoc(new_pos);
     }
 
     private void right() {
+        if ( (this.zero_index % this.size) == 0)
+            return;
 
+        int new_pos = this.zero_index - 1;
+        moveZeroToLoc(new_pos);
+    }
+
+    private boolean checkIfSolved () {
+        ArrayList<Integer> cpygame = new ArrayList<Integer>(tab);
+        cpygame.sort(null);
+
+        for (int i = 0; i < cpygame.size(); ++i) {
+            if (cpygame.get(i) != tab.get(i))
+                return false;
+        }
+        return true;
     }
 
     public boolean aplicarSolucao (String sol) {
+        this.printCurrentState();
+
         for (int i = 0; i < sol.length(); ++i) {
             var dengo = sol.charAt(i);
-            
+
             switch (dengo) {
                 case 'u':
                     this.up();    
@@ -89,7 +132,9 @@ public class Table {
                 default:
                     break;
             }
+
+            this.printCurrentState();
         }
-        return true;
+        return this.checkIfSolved();
     }
 }
