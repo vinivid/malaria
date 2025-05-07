98a88bdaf2b6e6489e7f65563c2143943a288d9b
Renan Banci Catarin <110069981+ReCaat@users.noreply.github.com>
2025-05-06 22:29:31 -0300

    ball

--- arvre/Main.java
+++ arvre/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner dog = new Scanner(System.in);
         ArvBin bin = new ArvBin(1000);
         ArvAvl avl = new ArvAvl(1000); 
+        ArvBal bal = new ArvBal(1000);
 
         while (dog.hasNext()) {
             String line = dog.nextLine();
@@ -13,15 +14,18 @@ public class Main {
             if (dem[0].equals("i")) {
                 bin.insert(dem[1]);
                 avl.insert(dem[1]);
+                bal.insert(dem[1]);
             } else if (dem[0].equals("d")) {
                 bin.remove(dem[1]);
                 avl.remove(dem[1]);
+                bal.remove(dem[1]);
             } else if (dem[0].equals("end")) {
                 break;
             }
         }
 
         System.out.println(bin);
+        System.out.println(bal);
         System.out.print(avl);
 
         dog.close();
